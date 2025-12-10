cef35ea2fcb2a5f870e3110f2b330ee7dceb884e
matan <matan@EL-ROEI>
2025-12-10 17:16:09 +0200

    r simple + clipping

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,11 @@ import numpy as np
 from audio import audio
 
 def BPM_preprocssing(audio, cadence):
-    r = audio.BPM / cadence.srm
+    r = cadence.BPM / audio.srm
+
+    r = 1.5 if r > 1.5 else r
+    r = 0.8 if r < 0.8 else r
+    
     return r
     
     
